c3cc6803ebb5738d8871b7fc6db016dab67f82d2
Igor Lima <igorlimaesilva94@gmail.com>
2022-05-04 14:05:44 -0300

    finalmente o bug do "\r" foi corrigido

--- tools.py
+++ tools.py
@@ -14,21 +14,109 @@ import qrcode
 from PIL import Image
 
 root = Tk()
+
+janela_width = 500
+janela_height = 500
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+x = (screen_width / 2) - (janela_width / 2)
+y = (screen_height / 2) - (janela_height / 2)
+
 root.resizable(False, False)
-root.geometry("500x500")
+root.geometry("500x500+" + str(int(x)) + "+" + str(int(y)))
 root.iconbitmap('icones/tools.ico')
 root.title("SEPATools")
 
-copia = ""
+
+def comparador():
+    comparador_janela = Toplevel(root)
+    comparador_janela.geometry("500x500+" + str(int(x)) + "+" + str(int(y)))
+    comparador_janela.resizable(False, False)
+    comparador_janela.iconbitmap("icones/compare.ico")
+    comparador_janela.title("Comparador")
+    comparador_janela.attributes("-topmost", True)
+
+    def limpar(texto, campo):
+        try:
+            Stringio = StringIO(texto)
+
+            df = pd.read_csv(Stringio)
+
+            df.sort_values(by=["Patrimônio"], inplace=True)
+
+            df["Patrimônio"] = df["Patrimônio"].apply(lambda x: '{0:0>6}'.format(x))
+
+            df1 = df[["Patrimônio"]]
+
+            df2 = df1.to_csv(header=None, index=False)
+
+            campo.delete("1.0", END)
+
+            campo.insert("1.0", str(df2).replace("\r", ""))
+
+        except Exception as e:
+            erro(e)
+
+    def comparar(texto1, texto2):
+
+        text_relacao_3.delete("1.0", END)
+
+        list1 = texto1.split("\n")
+        list2 = texto2.split("\n")
+
+        set1 = set(list1)
+        set2 = set(list2)
+
+        missing = list(sorted(set1 - set2))
+
+        print('missing:', missing)
+
+        for element in missing:
+            text_relacao_3.insert("1.0", element + "\n")
+
+    frame_master = Frame(comparador_janela)
+    frame_master.pack()
+
+    frame_1 = Frame(frame_master)
+    frame_1.pack(side=LEFT)
+
+    text_relacao_1 = Text(frame_1, width=20, height=28)
+    text_relacao_1.pack()
+
+    button_limpar_1 = Button(frame_1, text="Limpar", width=10, height=2,
+                             command=lambda: limpar(text_relacao_1.get("1.0", END), text_relacao_1))
+    button_limpar_1.pack(side=LEFT, pady=5)
+
+    frame_2 = Frame(frame_master)
+    frame_2.pack(side=LEFT)
+
+    text_relacao_2 = Text(frame_2, width=20, height=28)
+    text_relacao_2.pack()
+
+    button_limpar_2 = Button(frame_2, text="Limpar", width=10, height=2,
+                             command=lambda: limpar(text_relacao_2.get("1.0", END), text_relacao_2))
+    button_limpar_2.pack(side=LEFT, pady=5)
+
+    frame_3 = Frame(frame_master)
+    frame_3.pack(side=LEFT)
+
+    text_relacao_3 = Text(frame_3, width=20, height=28)
+    text_relacao_3.pack()
+
+    button_comparar = Button(frame_3, text="Comparar", width=10, height=2,
+                             command=lambda: comparar(text_relacao_1.get("1.0", END), text_relacao_2.get("1.0", END)))
+    button_comparar.pack(side=LEFT, pady=5)
+
+    button_limpar_3 = Button(frame_3, text="Para CSV", width=10, height=2)
+    button_limpar_3.pack(side=RIGHT, pady=5)
+
+    comparador_janela.mainloop()
 
 
 def apagar():
     text.delete("1.0", END)
-
-
-def copiar():
-    global copia
-    pyperclip.copy(copia)
 
 
 def erro(e):
@@ -112,7 +200,6 @@ def gerarQRCodeComBrasao():
 
 
 def apenasPatrimonio():
-    global copia
     try:
         Stringio = StringIO(text.get("1.0", END))
 
@@ -126,20 +213,19 @@ def apenasPatrimonio():
 
         df2 = df1.to_csv(header=None, index=False)
 
+        resultado = str(df2).replace("\r", "")
+
         text.delete("1.0", END)
 
-        text.insert("1.0", str(df2))
+        text.insert("1.0", resultado)
 
-        pyperclip.copy(str(df2))
-
-        copia = str(df2)
+        pyperclip.copy(resultado)
 
     except Exception as e:
         erro(e)
 
 
 def materialPatrimonio():
-    global copia
     try:
         Stringio = StringIO(text.get("1.0", END))
 
@@ -157,19 +243,18 @@ def materialPatrimonio():
 
         df2 = df1.to_csv(header=None, index=False)
 
+        resultado = str(df2).replace("\r", "")
+
         text.delete("1.0", END)
 
-        text.insert("1.0", str(df2))
+        text.insert("1.0", resultado)
 
         pyperclip.copy(str(df2))
-
-        copia = str(df2)
     except Exception as e:
         erro(e)
 
 
 def marcaPatrimonio():
-    global copia
     try:
         Stringio = StringIO(text.get("1.0", END))
 
@@ -183,25 +268,23 @@ def marcaPatrimonio():
 
         df2 = df1.to_csv(header=None, index=False)
 
+        resultado = str(df2).replace("\r", "")
+
         text.delete("1.0", END)
 
-        text.insert("1.0", str(df2))
+        text.insert("1.0", resultado)
 
         pyperclip.copy(str(df2))
-
-        copia = str(df2)
     except Exception as e:
         erro(e)
 
 
 def arquivoCSV():
-    global copia
-
     try:
         saida = filedialog.asksaveasfilename(defaultextension=".csv", filetypes=[("Arquivo CSV", "*.csv")])
 
         arquivo = open(saida, "wb")
-        arquivo.write(bytes(copia, "utf-8"))
+        arquivo.write(bytes(text.get("1.0", END), "utf-8"))
         arquivo.close()
 
         pergunta = messagebox.askyesno("Concluído", "Abrir arquivo?")
@@ -215,38 +298,39 @@ def arquivoCSV():
 
 menubar = Menu(root)
 
-menu1 = Menu(menubar, tearoff=0)
+menu_1 = Menu(menubar, tearoff=0)
 
-menu1.add_command(label="Mostrar QR code apenas", command=lambda: multithreading(apenasGerarQRCode))
-menu1.add_command(label="Gerar QR code sem brasão", command=lambda: multithreading(gerarQrCodeSemBrasao))
-menu1.add_command(label="Gerar QR code com brasão", command=lambda: multithreading(gerarQRCodeComBrasao))
+menu_1.add_command(label="Mostrar QR code apenas", command=lambda: multithreading(apenasGerarQRCode))
+menu_1.add_command(label="Gerar QR code sem brasão", command=lambda: multithreading(gerarQrCodeSemBrasao))
+menu_1.add_command(label="Gerar QR code com brasão", command=lambda: multithreading(gerarQRCodeComBrasao))
 
-menubar.add_cascade(label="QR Code", menu=menu1)
+menubar.add_cascade(label="QR Code", menu=menu_1)
 
-menu2 = Menu(menubar, tearoff=0)
+menu_2 = Menu(menubar, tearoff=0)
 
-menu2.add_command(label="Apenas patrimônio", command=lambda: multithreading(apenasPatrimonio))
-menu2.add_command(label="Material + Patrimônio", command=lambda: multithreading(materialPatrimonio))
-menu2.add_command(label="Marca + Patrimônio", command=lambda: multithreading(marcaPatrimonio))
+menu_2.add_command(label="Apenas patrimônio", command=lambda: multithreading(apenasPatrimonio))
+menu_2.add_command(label="Material + Patrimônio", command=lambda: multithreading(materialPatrimonio))
+menu_2.add_command(label="Marca + Patrimônio", command=lambda: multithreading(marcaPatrimonio))
 
-menubar.add_cascade(label="Padronizar", menu=menu2)
+menubar.add_cascade(label="Padronizar", menu=menu_2)
 
-menu3 = Menu(menubar, tearoff=0)
+menu_3 = Menu(menubar, tearoff=0)
 
-menu3.add_command(label="Abrir pastebin.com", command=lambda: multithreading(pasteBin))
-menu3.add_command(label="Salvar para um arquivo CSV", command=lambda: multithreading(arquivoCSV))
+menu_3.add_command(label="Abrir pastebin.com", command=lambda: multithreading(pasteBin))
+menu_3.add_command(label="Salvar para um arquivo CSV", command=lambda: multithreading(arquivoCSV))
+menu_3.add_command(label="Abrir Comparador", command=comparador)
 
-menubar.add_cascade(label="Outros", menu=menu3)
+menubar.add_cascade(label="Outros", menu=menu_3)
 
 root.config(menu=menubar)
 
 text = Text(root, height=27)
 text.pack(pady=5, padx=5)
 
-button_copiar = Button(root, text="Copiar", width=10, height=2, command=copiar)
+button_copiar = Button(root, text="Copiar", width=10, height=2, command=lambda: pyperclip.copy(text.get("1.0", END)))
 button_copiar.pack(side=LEFT, padx=5, pady=5)
 
-button_copiar = Button(root, text="Apagar", width=10, height=2, command=apagar)
+button_copiar = Button(root, text="Apagar", width=10, height=2, command=lambda: text.delete("1.0", END))
 button_copiar.pack(side=RIGHT, padx=5, pady=5)
 
 root.mainloop()
