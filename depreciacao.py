4379e9b11984342a513c37c55ee1ae43b1c0b2e2
None <None>
2022-10-14 11:06:41 -0300

    adicionado try e except block no calculo de depreciação, adicionado extensao para arquivos csv

--- depreciacao.py
+++ depreciacao.py
@@ -1,4 +1,4 @@
-from tkinter import filedialog, Toplevel, Frame, X, LEFT, RIGHT, Entry, Button, messagebox, END
+from tkinter import filedialog, Toplevel, Frame, X, LEFT, Entry, Button, messagebox, END
 import misc
 
 
@@ -12,61 +12,64 @@ def calcular(root, x, y, text):
 
     def iniciar():
 
-        if entry_lista_1 and entry_lista_2:
+        try:
+            if entry_lista_1 and entry_lista_2:
 
-            text.delete("1.0", "end")
+                text.delete("1.0", "end")
 
-            patrimonios_1 = []
+                patrimonios_1 = []
 
-            patrimonios_2 = []
+                patrimonios_2 = []
 
-            depreciacao_total = 0
+                depreciacao_total = 0
 
-            with open(entry_lista_1.get(), "r") as f1:
-                relacao_1 = f1.read()
+                with open(entry_lista_1.get(), "r") as f1:
+                    relacao_1 = f1.read()
 
-            with open(entry_lista_2.get(), "r") as f2:
-                relacao_2 = f2.read()
+                with open(entry_lista_2.get(), "r") as f2:
+                    relacao_2 = f2.read()
 
-            relacao_1_array = relacao_1.split("\n")
+                relacao_1_array = relacao_1.split("\n")
 
-            relacao_2_array = relacao_2.split("\n")
+                relacao_2_array = relacao_2.split("\n")
 
-            for i in relacao_1_array:
-                filtro_1 = i.split(",")
+                for i in relacao_1_array:
+                    filtro_1 = i.split(",")
 
-                if filtro_1[1] != "0":
-                    patrimonios_1.append(filtro_1[0])
+                    if filtro_1[1] != "0":
+                        patrimonios_1.append(filtro_1[0])
 
-            for j in relacao_2_array:
-                filtro_2 = j.split(",")
+                for j in relacao_2_array:
+                    filtro_2 = j.split(",")
 
-                if filtro_2[1] == "0":
-                    patrimonios_2.append(filtro_2[0])
+                    if filtro_2[1] == "0":
+                        patrimonios_2.append(filtro_2[0])
 
-            temp = [x for x in patrimonios_1 if x in patrimonios_2]
+                temp = [x for x in patrimonios_1 if x in patrimonios_2]
 
-            counter = 0
+                counter = 0
 
-            for valor in relacao_1_array:
-                filtro_3 = valor.split(",")
+                for valor in relacao_1_array:
+                    filtro_3 = valor.split(",")
 
-                if filtro_3[0] in temp:
-                    counter = counter + 1
+                    if filtro_3[0] in temp:
+                        counter = counter + 1
 
-                    depreciacao_total = depreciacao_total + float(filtro_3[1])
+                        depreciacao_total = depreciacao_total + float(filtro_3[1])
 
-                    text.insert("end", filtro_3[0] + " - R$ " + str(filtro_3[1]) + "\n")
+                        text.insert("end", filtro_3[0] + " - R$ " + str(filtro_3[1]) + "\n")
 
-            text.insert("1.0", "Total Achados: " + str(counter) + "\n\n")
+                text.insert("1.0", "Total Achados: " + str(counter) + "\n\n")
 
-            text.insert("end", "\nDepreciação Total: R$ " + str(round(depreciacao_total, 2)))
-        else:
-            messagebox.showerror("Erro", "Informe os dois arquivos.")
+                text.insert("end", "\nDepreciação Total: R$ " + str(round(depreciacao_total, 2)))
+            else:
+                messagebox.showerror("Erro", "Informe os dois arquivos.")
+        except Exception as e:
+            messagebox.showerror("Erro", str(e))
 
     def abrir(entry):
         entry.delete(0, END)
-        entry.insert(END, filedialog.askopenfilename())
+        entry.insert(END, filedialog.askopenfilename(defaultextension=".csv", filetypes=[("Arquivo CSV", "*.csv")]))
 
     frame1 = Frame(calcular_janela)
     frame1.pack(fill=X, pady=2)
