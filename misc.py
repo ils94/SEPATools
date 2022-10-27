9524838332f43b75d86427ef4a96a7ba61b5b2ed
None <None>
2022-10-27 11:08:20 -0300

    remoção de código

--- misc.py
+++ misc.py
@@ -1,20 +1,7 @@
 import threading
-import tabula
-from tkinter import messagebox, filedialog
 
 
 def multithreading(funcao):
     x = threading.Thread(target=funcao)
     x.setDaemon(True)
     x.start()
-
-
-def extrair_tabela():
-    try:
-        entrada = filedialog.askopenfilename(defaultextension=".csv", filetypes=[("Arquivo PDF", "*.pdf")])
-
-        if entrada != "":
-            tabula.convert_into(entrada, entrada + "_resultado.csv", output_format="csv", pages='all')
-
-    except Exception as e:
-        messagebox.showerror("Erro", str(e))
